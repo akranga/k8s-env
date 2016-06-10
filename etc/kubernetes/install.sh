7f3ae2b5076ed266ef40ba7a6e50e9ca76b2520d
Antons Kranga <akranga@accenture.com>
2016-06-10 18:25:11 +0300

    added master-node configs

--- etc/kubernetes/install.sh
+++ etc/kubernetes/install.sh
@@ -8,23 +8,35 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-
-manifests_dir="/etc/kubernetes/manifests"
-
 source "/etc/environment"
 
 function render_template() {
   sed -e "s|$private_ipv4|$COREOS_PRIVATE_IPV4|" \ 
       -e "s|$public_ipv4|$COREOS_PUBLIC_IPV4|" \
-      -e "s|$K8S_VER|$K8S_VER|" \
+      -e "s|${ADVERTISE_IP}|$COREOS_PUBLIC_IPV4|" \
+      -e "s|${ETCD_ENDPOINTS}|http://$COREOS_PUBLIC_IPV4:2379|" \
+      -e "s|${K8S_VER}|$K8S_VER|" \
       -i $1
 }
 
-function deploy_manifest {
-  yes | cp -rf "$DIR/$1" "$manifests_dir"
-  render_template "$manifests_dir/$1"
+function deploy_to_dir() {
+  mkdir -p "$2" | true
+  yes | cp -rf "$DIR/$1" "$2"
+  render_template "$2/$1"
 }
 
-mkdir -p "$manifests_dir"
+function deploy_manifest() {
+  deploy_to_dir "$1" "/etc/kubernetes/manifests"
+}
+
+function deploy_service() {
+  deploy_to_dir "$1" "/etc/systemd/system"
+}
 
 deploy_manifest "kube-apiserver.yaml"
+deploy_manifest "kube-proxy.yaml"
+deploy_manifest "kube-controller-manager.yaml"
+deploy_manifest "kube-scheduler.yaml"
+deploy_manifest "policy-agent.yaml"
+deploy_service  "calico-node.service"
+deploy_to_dir   "10-calico.conf" "/etc/kubernetes/cni/net.d"
